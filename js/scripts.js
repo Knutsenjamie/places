4ac3164f6e162aeaf884c3c64f5a97586fa1e154
Isaac Moreno <isaacmoreno@Isaacs-Mac-mini.local>
2021-04-05 11:59:42 -0700

    add bootstrap and jQuery to directory
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,16 @@ function States(city, landmarks, timeOfYear) {
 
 let Washington = new States("Seattle", ["The Needle", "Queen Anne"], "1995" )
 let Oregon = new States("Portland", ["Voodoo Donuts", "Pittock Mansion"], "2000")
-let Hawaii = ("Oahu", ["Waikiki", "Waimanalo Beach"], "2019")
-let California = ("San Diego", ["Balboa Park", "Seaworld"], "2017")
-let Illinois = ("Chicago", ["The Bean", "Museum of Art"], "2015");
+let Hawaii = new States ("Oahu", ["Waikiki", "Waimanalo Beach"], "2019")
+let California = new States ("San Diego", ["Balboa Park", "Seaworld"], "2017")
+let Illinois = new States ("Chicago", ["The Bean", "Museum of Art"], "2015");
 let Colorado = new States("Boulder", ["Mountain1", "Mountain2"], "2022");
 
+$(document).ready(function(){
+  $("form#stateForm").click(function(event) {
+  $("#Washington").html(States); 
+}
+)}
+)
 
 
