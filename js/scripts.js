5d9545d23ae17bd9956e4e2a404d34ed26950406
Isaac Moreno <isaacmoreno@Isaacs-Mac-mini.local>
2021-04-05 15:44:47 -0700

    add styling to doc
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,12 @@ function States(city, landmarks, timeOfYear) {
   this.timeOfYear = timeOfYear;
 }
 
-let Washington = new States("Seattle", ["The Needle", "Queen Anne"], "1995" )
-let Oregon = new States("Portland", ["Voodoo Donuts", "Pittock Mansion"], "2000")
-let Hawaii = new States ("Oahu", ["Waikiki", "Waimanalo Beach"], "2019")
-let California = new States ("San Diego", ["Balboa Park", "Seaworld"], "2017")
-let Illinois = new States ("Chicago", ["The Bean", "Museum of Art"], "2015");
-let Colorado = new States("Boulder", ["Mountain1", "Mountain2"], "2022");
-
+let Washington = new States("Seattle", ["The Needle" + ', ' + "Queen Anne"], "1995" )
+let Oregon = new States("Portland", ["Voodoo Donuts" + ', ' + "Pittock Mansion"], "2000")
+let Hawaii = new States ("Oahu", ["Waikiki" + ', ' + "Waimanalo Beach"], "2019")
+let California = new States ("San Diego", ["Balboa Park" + ', ' + "Seaworld"], "2017")
+let Illinois = new States ("Chicago", ["The Bean" + ', ' + "Museum of Art"], "2015");
+let Colorado = new States("Boulder", ["Mountain1" + ', ' + "Mountain2"], "2022");
 
 $(document).ready(function () {
   $("#Washington").click(function () {
